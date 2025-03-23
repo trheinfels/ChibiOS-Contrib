bc6a78acba73cceb4572cf2c6ca1abfa26a231ad
Tim Rheinfels <13381391+trheinfels@users.noreply.github.com>
2025-03-23 13:39:34 +0100

    hal/nrf52/gpiov1: fix errors when compiling without callbacks/wait

--- os/hal/ports/NRF5/LLD/GPIOv1/hal_pal_lld.h
+++ os/hal/ports/NRF5/LLD/GPIOv1/hal_pal_lld.h
@@ -403,7 +403,9 @@ typedef uint32_t iopadid_t;
 
 #if !defined(__DOXYGEN__)
 extern const PALConfig pal_default_config;
+#if (PAL_USE_WAIT == TRUE) || (PAL_USE_CALLBACKS == TRUE)
 extern palevent_t _pal_events[NRF5_GPIOTE_NUM_CHANNELS];
+#endif
 #endif
 
 #ifdef __cplusplus
